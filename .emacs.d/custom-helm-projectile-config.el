075e09cee99d0413309358fb7894f6370ab8da89
coquisx <coquisx@gmail.com>
2019-01-23 14:52:30 +0100

    Configure helm-projectile to identify files from custom project

--- .emacs.d/custom-helm-projectile-config.el
+++ .emacs.d/custom-helm-projectile-config.el
@@ -1,5 +1,12 @@
+(require 'projectile)
 (projectile-global-mode)
+
+;(setq projectile-indexing-method 'alien)
+;(setq projectile-indexing-method 'alien)
+(setq projectile-indexing-method 'native)
+
 (setq projectile-completion-system 'helm)
+(setq projectile-enable-caching nil)
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
 (helm-projectile-on)
