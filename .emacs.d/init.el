df72ddc6982f54d4e22055a2df475504d81376af
coquisx <coquisx@gmail.com>
2019-01-25 21:02:50 +0100

    init config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,58 +1,47 @@
-;; Fullscreen
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(require 'package)
+(add-to-list 'package-archives
+         '("melpa" . "http://melpa.org/packages/") t)
 
-;; Disable Menu Bar
-(menu-bar-mode -1)
+(package-initialize)
 
-;; Disable tool-bar
-(tool-bar-mode -1)
+(when (not package-archive-contents)
+    (package-refresh-contents))
 
-;; Disable startup-screen and message
-(setq inhibit-startup-screen t)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; Makes *scratch* empty.
-(setq initial-scratch-message "")
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-;; Removes *scratch* from buffer after the mode has been set.
-(defun remove-scratch-buffer ()
-  (if (get-buffer "*scratch*")
-      (kill-buffer "*scratch*")))
-(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
+(add-to-list 'load-path "~/.emacs.d/custom")
 
-;; Removes *messages* from the buffer.
-(setq-default message-log-max nil)
-(kill-buffer "*Messages*")
+(require 'setup-general)
+(if (version< emacs-version "24.4")
+    (require 'setup-ivy-counsel)
+  (require 'setup-helm)
+  (require 'setup-helm-gtags))
+; (require 'setup-ggtags)
+(require 'setup-cedet)
+(require 'setup-editing)
+(require 'setup-sr-speedbar)
 
-;; Removes *Completions* from buffer after you've opened a file.
-(add-hook 'minibuffer-exit-hook
-      '(lambda ()
-         (let ((buffer "*Completions*"))
-           (and (get-buffer buffer)
-                (kill-buffer buffer)))))
 
-;; Don't show *Buffer list* when opening multiple files at the same time.
-(setq inhibit-startup-buffer-menu t)
-
-;; Show only one active window when opening multiple files at the same time.
-(add-hook 'window-setup-hook 'delete-other-windows)
-
-;; No crees ficheros temporales xxx~
-(setq make-backup-files nil)    
-
-;; set keys to focus on another windows with rows and define prevous frame
-(global-set-key (kbd "C-x p") 'previous-multiframe-window)
-(global-set-key (kbd "C-x <up>") 'windmove-up)
-(global-set-key (kbd "C-x <down>") 'windmove-down)
-(global-set-key (kbd "C-x <right>") 'windmove-right)
-(global-set-key (kbd "C-x <left>") 'windmove-left)
-
-;; set number line to be show
-(global-linum-mode t)
-
-;; Set tango-dark theme
-
-(load-theme 'tango-dark t)
-
-;(set-face-attribute 'linum nil :background "#2e3436")
-
-;(set-face-attribute 'linum nil :background face-attribute 'default :background)
+;; function-args
+;; (require 'function-args)
+;; (fa-config-default)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ggtags zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- README.md
+++
@@ -1,4 +0,0 @@
-# emacs_configuration
-Emacs configuration project
-
-Adding ide features
